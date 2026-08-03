7f7647e2dd9ff5772bfb34cfd28a7c9d1e643f5e
Alex Wu <itsalexwu@gmail.com>
2026-08-03 14:05:37 +0200

    feat: refactor main function to use async and include clothing advice retrieval

--- src/whats_the_weather/main.py
+++ src/whats_the_weather/main.py
@@ -1,17 +1,22 @@
-import httpx
 import asyncio
-from dataclasses import dataclass
-from pprint import pprint
-import csv
 from whats_the_weather.getWeather import Weather
+from whats_the_weather.get_clothing_advice import get_clothing_advice
 
 ##https://ai.google.dev/gemini-api/docs/get-started
 
 url = "https://opendata-download-metfcst.smhi.se/api/category/snow1g/version/1/geotype/point/lon/18.2282/lat/59.3086/data.json"
+weather_csv_path = "src/whats_the_weather/weather.csv"
 
-def main(url: str):
+async def main(url: str):
     weather = Weather(url)
-    asyncio.run(weather.fetch_weather())
+    await weather.get_weather()
     print('fetching completed')
 
-main(url)
+    with open(weather_csv_path, "r") as f:
+        csv_data = f.read()
+
+    advice = get_clothing_advice(csv_data)
+    print(advice)
+
+if __name__ == "__main__":
+    asyncio.run(main(url))
